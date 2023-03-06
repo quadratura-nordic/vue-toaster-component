9fb56b749766f1cd6b006d2f5d414191f2c4e086
David Atanasoski <61380566+lulata@users.noreply.github.com>
2023-03-06 14:59:00 +0100

    reverting to default css code split

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,6 @@ export default defineConfig({
       name: "vue-toaster-component",
       fileName: "vue-toaster-component",
     },
-    cssCodeSplit: true,
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
