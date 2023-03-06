8622864906de6fcc6e8430b83e53fca88b74e881
David Atanasoski <61380566+lulata@users.noreply.github.com>
2023-03-06 11:22:25 +0100

    main.d.ts added

---
+++ dist/main.d.ts
@@ -0,0 +1,6 @@
+import ToastComponent from "./components/ToasterComponent.vue";
+import { useToaster } from "./components/Toaster";
+import { Plugin } from "vue";
+declare const _default: Plugin<any[]>;
+export default _default;
+export { useToaster, ToastComponent };
